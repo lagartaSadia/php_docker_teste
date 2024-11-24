e9ae03fa077c24b12756a3f044fb17d485d44242
lagartaSaida <campolino.l@gmail.com>
2024-11-24 01:17:13 -0300

    Testando cadastro de usuários

--- php/src/index.php
+++ php/src/index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Página PHP</title>
 </head>
+
 <body>
+
   <body>
     <?php include('./pages/itens.php') ?>
     <a href="./pages/login.php">Login</a>
+    <a href="./pages/cadastro.php">Cadastrar</a>
   </body>
 </body>
+
 </html>
--- php/src/pages/cadastro.php
+++ php/src/pages/cadastro.php
@@ -3,21 +3,26 @@
 include('../config/conexao.php');
 
 if (isset($_POST['email']) || isset($_POST['senha'])) {
-  if (strlen($_POST['email']) == 0) {
+  if (strlen($_POST['name']) == 0) {
+    echo "Preencha o nome";
+  } else if (strlen($_POST['email']) == 0) {
     echo "Preencha o e-mail";
   } else if (strlen($_POST['senha']) == 0) {
     echo "Preencha a senha";
   } else {
+    $nome = $conn->real_escape_string($_POST['name']);
     $email = $conn->real_escape_string($_POST['email']);
-    $senha = $conn->real_escape_string($_POST['senha']);
+    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
 
-    $sql_code = "SELECT * FROM usuarios WHERE email='$email' LIMIT 1";
+    $sql_code = "SELECT * FROM usuarios WHERE email='$email'";
     $sql_query = $conn->query($sql_code) or die("Falha na execução do código SQL: " . $conn->error);
 
     $quantidade = $sql_query->num_rows;
-    $usuario = $sql_query->fetch_assoc();
 
-    if (password_verify($senha, $usuario['senha'])) {
+    if ($quantidade == 0) {
+      $sql_code = "INSERT INTO usuarios(nome, email, senha) VALUES('$nome', '$email', '$senha')";
+      $conn->query($sql_code) or die("Falha na execução do código SQL: " . $conn->error);
+      $usuario = $sql_query->fetch_assoc();
 
       if (!isset($_SESSION)) {
         session_start();
@@ -28,7 +33,7 @@ if (isset($_POST['email']) || isset($_POST['senha'])) {
 
       header('Location: painel.php');
     } else {
-      echo "Falha ao logar! E-mail ou senha incorretos.";
+      echo "Falha ao cadastrar! E-mail já cadastrado.";
     }
   }
 }
@@ -41,18 +46,21 @@ if (isset($_POST['email']) || isset($_POST['senha'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
+  <title>cadastro</title>
 </head>
 
 <body>
   <?php include('../partials/navbar.php') ?>
   <form action="" method="post">
+    <label for="name">Nome</label>
+    <input type="text" name="name" id="name">
     <label for="email">E-mail</label>
     <input type="email" name="email" id="email">
     <label for="senha">Senha</label>
     <input type="password" name="senha" id="senha">
-    <button type="submit">Logar</button>
+    <button type="submit">Cadastrar</button>
   </form>
+
 </body>
 
 </html>
